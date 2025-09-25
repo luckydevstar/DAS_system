2282ff149ad9cf9f86dd3bfc195e5685171d6c30
melek <upwork.melek@gmail.com>
2025-09-25 01:16:25 -0300

    fix build error

--- app/(profile)/references/page.tsx
+++ app/(profile)/references/page.tsx
@@ -4,11 +4,14 @@ import ReferenceList from "@/components/profile/driving-info/references/referenc
 import ReferenceListItem from "@/components/profile/driving-info/references/reference-list/ReferenceListItem";
 import TabNav from "@/components/profile/driving-info/tab-nav/TabNav";
 import UserInfo from "@/components/profile/user-info/UserInfo";
+import { Suspense } from "react";
 
 const References = () => {
     return (
         <div className="max-w-7xl mx-auto py-9">
-            <ReferencesFilter />
+            <Suspense fallback={<div className="h-9 w-[260px]" />} >
+                <ReferencesFilter />
+            </Suspense>
             <div className="flex gap-8">
                 <UserInfo />
                 <div className="flex flex-col flex-1 gap-8">
