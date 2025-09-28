e9259adb1a1af642244f17e0d82795fb2167904a
melek <upwork.melek@gmail.com>
2025-09-28 13:51:14 -0300

    driving inductions

--- app/(profile)/accreditations/page.tsx
+++ app/(profile)/accreditations/page.tsx
@@ -1,5 +1,6 @@
 import CurrentProgress from "@/components/profile/driving-info/accreditations/current-progress/CurrentProgress";
 import DigitalBadegs from "@/components/profile/driving-info/accreditations/digital-badges/DigitalBadges";
+import DrvingInductions from "@/components/profile/driving-info/accreditations/driving-inductions/DrivingInductions";
 import TabNav from "@/components/profile/driving-info/tab-nav/TabNav";
 import UserInfo from "@/components/profile/user-info/UserInfo";
 import { Suspense } from "react";
@@ -17,6 +18,7 @@ const References = () => {
                     <div className="flex flex-col gap-8">
                         <DigitalBadegs />
                         <CurrentProgress />
+                        <DrvingInductions />
                     </div>
                 </div>
             </div>
---
+++ components/profile/driving-info/accreditations/driving-inductions/DrivingInductions.tsx
@@ -0,0 +1,121 @@
+"use client"
+
+import { Card } from "@/components/ui/card"
+import { Carousel, CarouselApi, CarouselContent, CarouselItem } from "@/components/ui/carousel";
+import React from "react";
+
+const drivingInductionData = [
+    {
+        image: "/images/di/di-asda.png",
+        company: "ASDA",
+        course: "Driver Induction",
+        revisionDate: "1 October 2025",
+        publishedBy: "Mel Rishmond",
+        vechileClass: "C1",
+    },
+    {
+        image: "/images/di/di-royal-mail.png",
+        company: "Royal Mail",
+        course: "Driver Induction",
+        revisionDate: "1 October 2025",
+        publishedBy: "Mel Rishmond",
+        vechileClass: "C1",
+    },
+    {
+        image: "/images/di/di-asda.png",
+        company: "ASDA",
+        course: "Driver Induction",
+        revisionDate: "1 October 2025",
+        publishedBy: "Mel Rishmond",
+        vechileClass: "C1",
+    },
+    {
+        image: "/images/di/di-royal-mail.png",
+        company: "Royal Mail",
+        course: "Driver Induction",
+        revisionDate: "1 October 2025",
+        publishedBy: "Mel Rishmond",
+        vechileClass: "C1",
+    },
+]
+
+const DrvingInductions = () => {
+    const [api, setApi] = React.useState<CarouselApi>()
+    const [current, setCurrent] = React.useState(0)
+    const [count, setCount] = React.useState(0)
+
+    React.useEffect(() => {
+        if (!api) {
+            return
+        }
+
+        setCount(api.scrollSnapList().length)
+        setCurrent(api.selectedScrollSnap() + 1)
+
+        api.on("select", () => {
+            setCurrent(api.selectedScrollSnap() + 1)
+        })
+    }, [api])
+    return (
+        <Card className="p-8 rounded-2xl gap-2">
+            <h3 className="text-2xl font-bold mt-4">Driving Inductions</h3>
+            <div className="py-4 px-10 grid grid-cols-2">
+                <div className="col-span-1">
+                    <Carousel
+                        opts={{
+                            loop: true,
+                        }}
+                        setApi={setApi}
+                    >
+                        <CarouselContent>
+                            {
+                                drivingInductionData.map((item, idx) => {
+                                    return (
+                                        <CarouselItem className="basis-1/2 px-8 flex justify-center" key={idx}>
+                                            <div>
+                                                <img src={item.image} className="w-full h-auto object-contain" />
+                                            </div>
+                                        </CarouselItem>
+                                    )
+                                })
+                            }
+                        </CarouselContent>
+                    </Carousel>
+                    <div className="w-full flex justify-center gap-2 mt-4 -ml-1">
+                    {
+                        drivingInductionData.map((_, idx) => {
+                            return (
+                                <div className={`w-2.5 h-2.5 ${idx === current - 1 ? 'bg-black' : 'bg-black/50'} rounded-full`} key={idx} />
+                            )
+                        })
+                    }
+                    </div>
+                </div>
+                <div className="col-span-1 px-8 flex flex-col gap-2">
+                    <div className="text-xl">
+                        <span className="font-bold mr-2">COMPANY:</span>
+                        <span>{drivingInductionData[current - 1]?.company}</span>
+                    </div>
+                    <div className="text-xl">
+                        <span className="font-bold mr-2">COURSE:</span>
+                        <span>{drivingInductionData[current - 1]?.course}</span>
+                    </div>
+                    <div className="text-xl">
+                        <span className="font-bold mr-2">REVISION DATE:</span>
+                        <span>{drivingInductionData[current - 1]?.revisionDate}</span>
+                    </div>
+                    <div className="text-xl">
+                        <span className="font-bold mr-2">PUBLISHED BY:</span>
+                        <span>{drivingInductionData[current - 1]?.publishedBy}</span>
+                    </div>
+                    <div className="text-xl">
+                        <span className="font-bold mr-2">VEHICLE CLASS:</span>
+                        <span>{drivingInductionData[current - 1]?.vechileClass}</span>
+                    </div>
+                </div>
+            </div>
+        </Card>
+    )
+}
+
+export default DrvingInductions;
