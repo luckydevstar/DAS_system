8b0bbfe46645b8fe848df47b7ddc2c048aef5b05
melek <upwork.melek@gmail.com>
2025-09-29 15:32:48 -0300

    fix: make changes to default page as well

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,22 @@ import DrivingFootprint from "@/components/profile/driving-info/experience/drivi
 import HighwayTypeDriving from "@/components/profile/driving-info/experience/highway-type-driving/HighwayTypeDriving";
 import TotalHoursGauge from "@/components/profile/driving-info/experience/total-hours-guage/TotalHoursGauge";
 import OperationalHours from "@/components/profile/driving-info/experience/operational-hours/OperationalHours";
-import TabNav from "@/components/profile/driving-info/tab-nav/TabNav";
-import Filter from "@/components/profile/driving-info/experience/filter/ExperienceFilter";
 import UserInfo from "@/components/profile/user-info/UserInfo";
+import Nav from "@/components/profile/nav/Nav";
+import ExperienceFilter from "@/components/profile/driving-info/experience/filter/ExperienceFilter";
 
 const Page = () => {
   return (
     <div className="bg-[#fafafa]">
       <Header />
-      <div className="max-w-7xl mx-auto py-9">
-        <Filter />
-        <div className="flex gap-8">
+      <div className="h-[160px] bg-[#76A0B5]" />
+      <Nav>
+        <ExperienceFilter />
+      </Nav>
+      <div className="max-w-7xl mx-auto">
+        <div className="flex gap-8 w-full">
           <UserInfo />
-          <div className="flex flex-col flex-1 gap-8">
-            <TabNav />
+          <div className="flex flex-col gap-8 w-full">
             <div className="flex gap-8">
               <TotalHoursGauge day={3424} night={1078} />
               <HighwayTypeDriving day={3424} night={1078} />
