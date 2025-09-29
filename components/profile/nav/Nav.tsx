0ff0b42791a0aef9cf50fa9ea87d256aab4850ba
melek <upwork.melek@gmail.com>
2025-09-29 15:36:29 -0300

    fix: update nav style with width

--- components/profile/nav/Nav.tsx
+++ components/profile/nav/Nav.tsx
@@ -3,17 +3,20 @@ import TabNav from "../driving-info/tab-nav/TabNav";
 
 const Nav = ({ children }: { children: React.ReactNode }) => {
     return (
-        <div className="max-w-7xl mx-auto sticky top-[90px] bg-[#FAFAFA] pt-4 py-8 flex gap-8 z-40">
-            <div className="flex flex-col justify-center gap-2 items-center bottom-full w-80 shrink-0 -mt-28">
-                <div className="rounded-full w-34 h-34 border border-white">
-                    <img src="/images/avatar.jpg" alt="avatar" className="w-full h-full object-cover rounded-full" />
+        <div className="w-full sticky top-[90px] bg-[#FAFAFA]  z-40">
+            <div className="max-w-7xl mx-auto pt-4 py-8 flex gap-8">
+                <div className="flex flex-col justify-center gap-2 items-center bottom-full w-80 shrink-0 -mt-28">
+                    <div className="rounded-full w-34 h-34 border border-white">
+                        <img src="/images/avatar.jpg" alt="avatar" className="w-full h-full object-cover rounded-full" />
+                    </div>
+                    <h3 className="text-2xl font-semibold">Simon Sample</h3>
                 </div>
-                <h3 className="text-2xl font-semibold">Simon Sample</h3>
+                <div className="flex-1 flex flex-col">
+                    {children}
+                    <TabNav />
+                </div>
             </div>
-            <div className="flex-1 flex flex-col">
-                {children}
-                <TabNav />
-            </div>
+
         </div>
     );
 };
